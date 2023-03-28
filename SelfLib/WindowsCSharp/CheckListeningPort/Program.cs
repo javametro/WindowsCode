003bbadc0bccdc0db87ba37e18bc6f366353bb08
wangpeng17 <wangpeng17@lenovo.com>
2023-03-28 10:04:03 +0800

    new project WindowsCSharp

---
+++ SelfLib/WindowsCSharp/CheckListeningPort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckListeningPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int portNum = 28262;
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+            foreach(IPEndPoint endpoint in tcpConnInfoArray)
+            {
+                if(endpoint.Port == portNum)
+                {
+                    Console.WriteLine($"port {portNum} is listending");
+                }
+            }
+        }
+    }
+}
